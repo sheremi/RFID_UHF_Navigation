d08f727a8b3ecc7db82bd6ba4dea1c7e256a08c3
unknown <lyavinskova@.like.e-technik.uni-erlangen.de>
2011-06-10 20:39:22 +0200

    Reworked serial port preferences activity. 	modified:   src/de/unierlangen/like/ui/SerialPortPreferences.java

--- src/de/unierlangen/like/ui/SerialPortPreferences.java
+++ src/de/unierlangen/like/ui/SerialPortPreferences.java
@@ -1,31 +1,14 @@
-
-
-/*
- * Copyright 2009 Cedric Priscal
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. 
- */
-
 package de.unierlangen.like.ui;
 
 import java.io.IOException;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -33,8 +16,17 @@ import android.view.MenuItem;
 import de.unierlangen.like.serialport.SerialPortFinder;
 import de.unierlangen.like.usb.FreerunnerUSB;
 
-public class SerialPortPreferences extends PreferenceActivity implements OnPreferenceChangeListener {
-
+public class SerialPortPreferences extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+	
+	public static final String KEY_DEVICES_PREFERENCE = "DEVICE";
+	public static final String KEY_BAUDRATES_PREFERENCE = "BAUDRATE";
+    public static final String KEY_USBHOST_PREFERENCE = "USBhost";
+	
+	private ListPreference devices;
+	private ListPreference baudrates;
+	private CheckBoxPreference usbHost;
+	//private SharedPreferences sharedPreferences;
+	
 	/**
 	 * Interface
 	 */
@@ -58,7 +50,7 @@ public class SerialPortPreferences extends PreferenceActivity implements OnPrefe
 
 		
 		// Devices
-		final ListPreference devices = (ListPreference)findPreference("DEVICE");
+		devices = (ListPreference)findPreference("DEVICE");
         
 		
 		mSerialPortFinder = new SerialPortFinder();
@@ -66,30 +58,28 @@ public class SerialPortPreferences extends PreferenceActivity implements OnPrefe
         String[] entryValues = mSerialPortFinder.getAllDevicesPath();
 		devices.setEntries(entries);
 		devices.setEntryValues(entryValues);
-		devices.setSummary(devices.getValue());
-		devices.setOnPreferenceChangeListener(this);
+		
 		
 		// Baud rates
-		final ListPreference baudrates = (ListPreference)findPreference("BAUDRATE");
-		baudrates.setSummary(baudrates.getValue());
-		baudrates.setOnPreferenceChangeListener(this);
+		baudrates = (ListPreference)findPreference("BAUDRATE");
 		
 		//USB
-		final CheckBoxPreference usbHost = (CheckBoxPreference)findPreference("USBhost");
-		usbHost.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-			
-			public boolean onPreferenceChange(Preference preference, Object object) {
-/*				try {
-					FreerunnerUSB.setUSBHostState(usbHost.isChecked());
-				} catch (IOException e) {
-					showMsg("Access to driver denied");					
-					e.printStackTrace();
-				}*/
-				return true;
-			}
-		});
-		
+		usbHost = (CheckBoxPreference)findPreference("USBhost");
+				
 	}
+	
+	 @Override
+	    protected void onResume() {
+	        super.onResume();
+
+	        // Setup the initial values
+	        devices.setSummary("Current value is " + devices.getValue());
+	        baudrates.setSummary("Current value is " + baudrates.getValue());
+	        //usbHost.setSummary(sharedPreferences.getBoolean(key, false) ? "Disable this setting" : "Enable this setting"); 
+	        
+	        // Set up a listener whenever a key changes            
+	        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	    }
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu){
@@ -121,15 +111,35 @@ public class SerialPortPreferences extends PreferenceActivity implements OnPrefe
 	
 	@Override
 	protected void onPause() {
+		
+		super.onPause();
+
+        // Unregister the listener whenever a key changes            
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);    
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        // Let's do something a preference value changes
+        if (key.equals(KEY_DEVICES_PREFERENCE)) {
+          devices.setSummary("Current value is " + devices.getValue());
+        }
+        else if (key.equals(KEY_BAUDRATES_PREFERENCE)) {
+          baudrates.setSummary("Current value is " + baudrates.getValue()); 
+        }
+        else if (key.equals(KEY_USBHOST_PREFERENCE)) {
+            usbHost.setSummary(sharedPreferences.getBoolean(key, false) ? "Disable this setting" : "Enable this setting"); 
+          }
+   		
 		try {
 			FreerunnerUSB.setUSBHostState(false);
 		} catch (IOException e) {
-			
+			//showMsg("Access to driver denied");					
 			e.printStackTrace();
 		}
 		
 		super.onPause();
 	}
+
 	
 }
 
