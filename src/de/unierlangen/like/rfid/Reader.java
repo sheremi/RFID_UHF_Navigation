4d900450205bdfa9a138b8a71fc4d24d1336718b
Ekaterina Lyavinskova <kate.lyavinskova@gmail.com>
2011-09-22 20:37:33 +0200

    Changed Reader
    Implemented OnStringReceivedListener,
    changed wait time for receiving thread
    
    	modified:   src/de/unierlangen/like/rfid/Reader.java

--- src/de/unierlangen/like/rfid/Reader.java
+++ src/de/unierlangen/like/rfid/Reader.java
@@ -1,7 +1,6 @@
 package de.unierlangen.like.rfid;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,12 +8,17 @@ import java.util.Iterator;
 import java.util.regex.Pattern;
 
 import android.content.SharedPreferences;
+import android.util.Log;
+import de.unierlangen.like.serialport.OnStringReceivedListener;
 import de.unierlangen.like.serialport.SerialPort;
 
-public class Reader {
+public class Reader implements OnStringReceivedListener{
 
-	SerialPort readerSerialPort;
+	private static final String TAG = "Reader";
+	SerialPort readerSerialPort;	
 	private String amountOfTags;
+	private String response;
+	//private Lock mLock;
 	
 	public static enum Configuration {DEFAULT, LOW_POWER, HIGH_POWER};
 	/**
@@ -29,8 +33,11 @@ public class Reader {
 		String path = sp.getString("DEVICE", "");
 		int baudrate = Integer.decode(sp.getString("BAUDRATE", "-1"));
 		readerSerialPort = new SerialPort(path, baudrate);
-		readerSerialPort.writeString("PREVED");
-		/*String response = readerSerialPort.readString();
+		readerSerialPort.setOnStringReceivedListener(this);
+		synchronized (this) {
+			readerSerialPort.writeString("PREVED");
+			this.wait(700);		
+		}
 		
 		if (response.contains("MEDVED")){
 			//everything is fine
@@ -41,7 +48,7 @@ public class Reader {
 			throw new IOException("Reader MCU behaves odd, check baudrate: " + response);
 		} else {
 			throw new IOException("No response from reader");
-		}*/
+		}
 	}
 	
 	public void initialize(Configuration configuration) throws IOException{
@@ -62,17 +69,29 @@ public class Reader {
 		//Create container
 		ArrayList<GenericTag> tags = new ArrayList<GenericTag>();
 		//Tell reader MCU to start inventory round
-		//readerSerialPort.writeString("z");
-		String tagsString = "tags,3,EBA123,14,FA894,1,BEEF666,30";//readerSerialPort.readString();
-		if (tagsString.contains("error")) {
-			throw new IOException("Reader MCU reported error:" + tagsString);
+		//String tagsString = "tags,3,EBA123,14,FA894,1,BEEF666,30";
+		//readerSerialPort.readString();
+		synchronized (this) {
+			response="";
+			readerSerialPort.writeString("req r tags");
+			//mLock.wait();
+			try {
+				this.wait(700);
+			} catch (InterruptedException e) {
+				Log.e(TAG, "Unexpected interrupt",e);
+			}
+			if (response.contains("error")) {
+				throw new IOException("Reader MCU reported error:" + response);
+			}	
 		}
+
 		Pattern oneElement = Pattern.compile(",");
-		ArrayList<String> strings = new ArrayList<String>(Arrays.asList(oneElement.split(tagsString)));
+		ArrayList<String> strings = new ArrayList<String>(Arrays.asList(oneElement.split(response)));
 		Iterator<String> iterator = strings.iterator();
-		//skip first element
-		iterator.next();
-		//skip second element
+		// Check if string header is correct
+		if (iterator.next()!="resp r tags")
+			throw new IOException("response should start with 'resp r tags'");
+		// TODO use amountOfTags where it should be used
 		amountOfTags = iterator.next();
 		while (iterator.hasNext()){
 			tags.add(new GenericTag(iterator.next(),Integer.parseInt(iterator.next()), true));
@@ -81,15 +100,44 @@ public class Reader {
 		return tags;
 	}
 	
-	public String displayRegisters() throws IOException, InterruptedException{
-		readerSerialPort.writeString("q");
-		//FIXME this is not the way it should be done, use listener
-		String registersString = readerSerialPort.readString();
-		return registersString;
+	public ArrayList<String> displayRegisters() throws IOException, InterruptedException{
+		// TODO create class for registers
+		synchronized (this) {
+			response="";
+			readerSerialPort.writeString("req r regs");
+			try {
+				this.wait(700);
+			} catch (InterruptedException e) {
+				Log.e(TAG, "Unexpected interrupt",e);
+			}
+		}
+		if (response.contains("error")) {
+			throw new IOException("Reader MCU reported error:" + response);
+		}
+		Pattern oneElement = Pattern.compile(",");
+		ArrayList<String> strings = new ArrayList<String>(Arrays.asList(oneElement.split(response)));
+		// Check if string header is correct
+		if (strings.remove(0)!="resp r regs")
+			throw new IOException("response should start with 'resp r regs'");
+		
+		return strings;
 	}
 	
 	public String getAmountOfTags() {
 		return amountOfTags;
 	}
 
+	/**
+	 * Called when {@link de.unierlangen.like.serialport.SerialPort SerialPort}
+	 * receives string from serial port. Stores response and unlocks the thread.
+	 */
+	public void onStringReceived(String string) {
+		synchronized (this) {
+			response = response.concat(string);
+			if (response.contains("\n")){
+				Log.d(TAG, "Response: "+response);
+				this.notify();	
+			}
+		}
+	}
 }
