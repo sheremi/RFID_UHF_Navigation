3d7a9511d0273721de4415097d93f92899edc0cb
Ekaterina Lyavinskova <kate.lyavinskova@gmail.com>
2012-02-19 23:58:58 +0100

    Zone: Optimization
    	modified:   src/de/unierlangen/like/navigation/Zone.java

--- src/de/unierlangen/like/navigation/Zone.java
+++ src/de/unierlangen/like/navigation/Zone.java
@@ -4,18 +4,40 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import android.graphics.PointF;
+import android.util.Log;
 
 public class Zone {
+	private static final String TAG = "Zone";
+	private static final boolean DBG = true;
 	private static final int AMOUNT_OF_POINTS_PER_ZONE = 72;
 	private ArrayList<PointF> points;
 	private static Zone instance;
 	
 	//Constructors
 	public Zone(Tag tag, float radius) {
+		if (instance == null) {
+			// if there is no static instance yet, create it
+			instance = new Zone();
+		}
+		points = new ArrayList<PointF>(AMOUNT_OF_POINTS_PER_ZONE);
+		// for each point set radius by multiplying point.x and point.y
+		// and make a coordinate translation by tag.x and tag.y
+		for (PointF point : instance.getPoints()) {
+			float x = point.x*radius+tag.x;
+			float y = point.y*radius+tag.y;
+			points.add(new PointF(x, y));
+		}
+	}
+
+	/**
+	 * Creates a zone with center in (0f, 0f) and radius of 1f
+	 */
+	private Zone() {
+		if (DBG) Log.d (TAG, "Creating a zone with center in (0f, 0f) and radius of 1f");
 		points = new ArrayList<PointF>(AMOUNT_OF_POINTS_PER_ZONE);
 		for (int i=0; i<AMOUNT_OF_POINTS_PER_ZONE; i++){
-			float x = (float)(tag.getX()+radius*Math.cos((i*6.283f)/AMOUNT_OF_POINTS_PER_ZONE));
-			float y = (float)(tag.getY()+radius*Math.sin((i*6.283f)/AMOUNT_OF_POINTS_PER_ZONE));
+			float x = (float)(Math.cos((i*6.283)/AMOUNT_OF_POINTS_PER_ZONE));
+			float y = (float)(Math.sin((i*6.283)/AMOUNT_OF_POINTS_PER_ZONE));
 			points.add(new PointF(x, y));
 		}
 	}
