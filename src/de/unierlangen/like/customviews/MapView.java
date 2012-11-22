0bfa82d764d2377d3eff34dc1bdfd6fc334bafe9
Ekaterina <kate.lyavinskova@gmail.com>
2012-11-22 22:08:33 +0100

    Working on new map and it's scaling
    	modified:   AndroidManifest.xml
    	modified:   src/de/unierlangen/like/customviews/MapView.java
    
    	new file:   res/drawable/map_small.jpg

--- src/de/unierlangen/like/customviews/MapView.java
+++ src/de/unierlangen/like/customviews/MapView.java
@@ -43,6 +43,12 @@ public class MapView extends View {
     private final Logger log = Logger.getDefaultLogger();
 
     // Drawing tools
+    private Paint backgroundPaint;
+    private Bitmap background;
+    private Matrix backgroundMatrix;
+    /*
+     * private Matrix backgroundMatrix; private float backgroundScale;
+     */
     private Paint debugRectPaint;
     private Paint tagPaint;
     private Paint wallsPaint;
@@ -62,7 +68,7 @@ public class MapView extends View {
     private Path routingPath;
     private PointF readerPosition;
     // Items to translate and scale
-    private float padding = 3.0f;
+    private float padding = 10.0f;
 
     // Constructors
     public MapView(Context context) {
@@ -102,6 +108,14 @@ public class MapView extends View {
     // Methods
     private void init() {
         // initialize all fields as empty
+        backgroundPaint = new Paint();
+        backgroundPaint.setFilterBitmap(true);
+        // background =
+        // BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().getPath()
+        // + "/like/map_small.jpg");
+        background = BitmapFactory
+                .decodeResource(getContext().getResources(), R.drawable.map_small);
+
         walls = new ArrayList<Wall>();
         doors = new ArrayList<Door>();
         tags = new ArrayList<Tag>();
@@ -245,8 +259,17 @@ public class MapView extends View {
         canvas.translate(positionX, positionY);
     }
 
-    private void drawTag(Canvas canvas, Paint paint, Tag tag) {
+    private void drawBackground(Canvas canvas) {
+        if (background == null) {
+            Logger.w("Background not created");
+        } else {
+            // canvas.drawBitmap(background, 0, 0, backgroundPaint);
+            canvas.drawBitmap(background, null, new RectF(0.f, -40.0f, 80.0f, 70.0f),
+                    backgroundPaint);
+        }
+    }
 
+    private void drawTag(Canvas canvas, Paint paint, Tag tag) {
         int filterColor = 0;
         /** Calculates value relative to range */
         float position;
@@ -358,10 +381,11 @@ public class MapView extends View {
         // MeasureSpec.UNSPECIFIED) {heightSize=300;}
         setMeasuredDimension(widthSize, heightSize);
         //
-        preDrawnMap = new Picture();
-        Canvas mapCanvas = preDrawnMap.beginRecording(widthSize, heightSize);
-        drawMap(mapCanvas);
-        preDrawnMap.endRecording();
+        /*
+         * preDrawnMap = new Picture(); Canvas mapCanvas =
+         * preDrawnMap.beginRecording(widthSize, heightSize);
+         * drawMap(mapCanvas); preDrawnMap.endRecording();
+         */
     }
 
     @Override
@@ -371,6 +395,8 @@ public class MapView extends View {
 
         /** Calculate drawing area, using counted tags' position. */
         prepareDrawingArea(canvas);
+
+        drawBackground(canvas);
         /** Draw debug rectangle */
         // canvas.drawRect(rectFTags, debugRectPaint);
         /** Draw zones around tags */
@@ -381,7 +407,9 @@ public class MapView extends View {
             }
         }
         /** Draw map */
-        canvas.drawPicture(preDrawnMap);
+        drawMap(canvas);
+        // canvas.drawPicture(preDrawnMap);
+
         /** Draw tags */
         for (Tag tag : tags) {
             drawTag(canvas, tagPaint, tag);
