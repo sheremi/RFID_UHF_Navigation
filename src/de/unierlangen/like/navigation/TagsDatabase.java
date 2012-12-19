7d2f6efc261b49805338cdf6007b50f37cef65c6
Ekaterina <kate.lyavinskova@gmail.com>
2012-12-19 15:14:11 +0100

    Added new Tags
    this closes #2
    
    	modified:   src/de/unierlangen/like/navigation/TagsDatabase.java

--- src/de/unierlangen/like/navigation/TagsDatabase.java
+++ src/de/unierlangen/like/navigation/TagsDatabase.java
@@ -46,24 +46,60 @@ public class TagsDatabase {
         ArrayList<TagOnTheWall> tagsOnTheWall = new ArrayList<TagsDatabase.TagOnTheWall>();
         TagOnTheWall newTag;
 
-        newTag = new TagOnTheWall(30.00f, 7.25f, "1");
+        newTag = new TagOnTheWall(31.00f, 7.15f, "1");
         newTag.addEPC("308033b2ddd96400000");
         newTag.addEPC("b08033b2ddd96400000");
 
         newTag.addEPC("");
         tagsOnTheWall.add(newTag);
 
-        newTag = new TagOnTheWall(24.68f, 5.67f, "2");
+        newTag = new TagOnTheWall(24.68f, 5.77f, "2");
         newTag.addEPC("50033b2ddd96400000");
         tagsOnTheWall.add(newTag);
 
-        newTag = new TagOnTheWall(32.00f, 4.25f, "3");
+        newTag = new TagOnTheWall(35.00f, 5.77f, "3");
         newTag.addEPC("305fb63ac1f3841ec88467");
         tagsOnTheWall.add(newTag);
 
         // tags from Andreas
-        newTag = new TagOnTheWall(32.00f, 4.25f, "4");
+        newTag = new TagOnTheWall(33.40f, 8.25f, "4");
         newTag.addEPC("30833b2ddd96400000");
+        tagsOnTheWall.add(newTag);
+
+        newTag = new TagOnTheWall(35.10f, 7.15f, "5");
+        newTag.addEPC("a1b133b2ddd96400000");
+        tagsOnTheWall.add(newTag);
+
+        newTag = new TagOnTheWall(37.60f, 5.77f, "6");
+        newTag.addEPC("a2b233b2ddd96400000");
+        tagsOnTheWall.add(newTag);
+
+        newTag = new TagOnTheWall(40.00f, 7.15f, "7");
+        newTag.addEPC("a3b333b2ddd96400000");
+        tagsOnTheWall.add(newTag);
+
+        newTag = new TagOnTheWall(40.50f, 5.77f, "8");
+        newTag.addEPC("a4b433b2ddd96400000");
+        tagsOnTheWall.add(newTag);
+
+        newTag = new TagOnTheWall(42.20f, 5.77f, "9");
+        newTag.addEPC("a5b533b2ddd96400000");
+        tagsOnTheWall.add(newTag);
+
+        newTag = new TagOnTheWall(44.00f, 7.15f, "10");
+        newTag.addEPC("a6b633b2ddd96400000");
+        tagsOnTheWall.add(newTag);
+
+        newTag = new TagOnTheWall(54.00f, 5.20f, "11");
+        newTag.addEPC("a7b733b2ddd96400000");
+        tagsOnTheWall.add(newTag);
+
+        newTag = new TagOnTheWall(48.00f, 5.77f, "12");
+        newTag.addEPC("a8b833b2ddd96400000");
+        tagsOnTheWall.add(newTag);
+
+        newTag = new TagOnTheWall(49.50f, 5.77f, "13");
+        newTag.addEPC("a9b933b2ddd96400000");
         tagsOnTheWall.add(newTag);
 
         // Emulation tags
