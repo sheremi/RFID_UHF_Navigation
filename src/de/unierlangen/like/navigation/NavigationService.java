1cf525a2c183a6244382d1d4df4b05052d03c7ce
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2013-01-01 19:06:27 +0100

    Calculate route in background
    	modified:   src/de/unierlangen/like/navigation/NavigationService.java

--- src/de/unierlangen/like/navigation/NavigationService.java
+++ src/de/unierlangen/like/navigation/NavigationService.java
@@ -9,7 +9,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.PointF;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
 import android.os.Parcelable;
 import android.widget.Toast;
 
@@ -18,7 +22,10 @@ import com.github.androidutils.logger.Logger;
 import de.unierlangen.like.Intents;
 import de.unierlangen.like.rfid.GenericTag;
 
-public class NavigationService extends Service {
+public class NavigationService extends Service implements Handler.Callback {
+    // private static final int
+
+    private static final int FIND_ROUTE = 1;
     private static final float ZONE_RADIUS = 2.0f;
     private final Logger log = Logger.getDefaultLogger();
     private MapBuilder mapBuilder;
@@ -28,6 +35,7 @@ public class NavigationService extends Service {
     private ArrayList<Tag> tags;
     private PointF destination;
     private PointF currentPosition;
+    private Handler mHandler;
 
     @Override
     public void onCreate() {
@@ -48,6 +56,10 @@ public class NavigationService extends Service {
 
         IntentFilter filter = new IntentFilter(Intents.ACTION_TAGS);
         registerReceiver(readerReceiver, filter);
+        HandlerThread thread = new HandlerThread("NavigationServiceThread");
+        thread.start();
+        Looper looper = thread.getLooper();
+        mHandler = new Handler(looper, this);
     }
 
     private final BroadcastReceiver readerReceiver = new BroadcastReceiver() {
@@ -63,19 +75,20 @@ public class NavigationService extends Service {
     };
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public int onStartCommand(final Intent intent, int flags, int startId) {
         log.d(intent.getAction());
         if (intent.getAction().equals(Intents.ACTION_SET_DESTINATION)) {
             destination = intent.getParcelableExtra(Intents.EXTRA_DESTINATION);
             if (currentPosition != null) {
-                ArrayList<PointF> route = dijkstraRouter.findRoute(currentPosition, destination);
-                broadcastValues(Intents.ACTION_ROUTE_FOUND, Intents.EXTRA_ROUTE, route);
+                findRouteInBackGround();
             }
 
         } else if (Intents.ACTION_TAGS.equals(intent.getAction())) {
-            // since now we know that Intent action is ACTION_TAGS, we know
+            // since now we know that Intent action is ACTION_TAGS, we
+            // know
             // that
-            // array of tags is attached to the intent as an extra with key
+            // array of tags is attached to the intent as an extra with
+            // key
             // EXTRA_TAGS
             ArrayList<GenericTag> readTagsFromReader = intent
                     .getParcelableArrayListExtra(Intents.EXTRA_TAGS);
@@ -86,12 +99,9 @@ public class NavigationService extends Service {
                 navigation.setTags(newTags);
                 currentPosition = navigation.getReaderPosition();
                 if (destination != null) {
-                    ArrayList<PointF> routingPath = dijkstraRouter.findRoute(currentPosition,
-                            destination);
-                    broadcastValues(Intents.ACTION_ROUTE_FOUND, Intents.EXTRA_ROUTE, routingPath);
+                    findRouteInBackGround();
                 }
-                broadcastValues(Intents.ACTION_TAGS_ON_WALLS, Intents.EXTRA_TAGS_ON_WALLS,
-                        newTags);
+                broadcastValues(Intents.ACTION_TAGS_ON_WALLS, Intents.EXTRA_TAGS_ON_WALLS, newTags);
                 broadcastValues(Intents.ACTION_ZONES, Intents.EXTRA_ZONES,
                         navigation.getZones(ZONE_RADIUS));
                 broadcast(Intents.ACTION_LOCATION_FOUND, Intents.EXTRA_POSITION, currentPosition);
@@ -100,6 +110,26 @@ public class NavigationService extends Service {
             log.d("Navigation start was requested");
         }
         return START_STICKY;
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        switch (msg.what) {
+        case FIND_ROUTE:
+            ArrayList<PointF> routingPath = dijkstraRouter.findRoute(currentPosition, destination);
+            broadcastValues(Intents.ACTION_ROUTE_FOUND, Intents.EXTRA_ROUTE, routingPath);
+            break;
+
+        default:
+            break;
+        }
+        return false;
+    }
+
+    private void findRouteInBackGround() {
+        // TODO make sure we are right
+        mHandler.removeMessages(1);
+        mHandler.sendEmptyMessage(1);
     }
 
     private void broadcast(String action, String extra, Parcelable value) {
