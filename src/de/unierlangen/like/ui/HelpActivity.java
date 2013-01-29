568466810dddcf97414962af9b181caad704d632
Ekaterina <kate.lyavinskova@gmail.com>
2013-01-29 16:39:23 +0100

    Added ActionBar to Guide
    	modified:   res/menu/menu.xml
    	modified:   src/de/unierlangen/like/ui/HelpActivity.java
    
    	modified:   res/menu/menu.xml
    	new file:   res/menu/menu_help.xml

--- src/de/unierlangen/like/ui/HelpActivity.java
+++ src/de/unierlangen/like/ui/HelpActivity.java
@@ -5,22 +5,31 @@ import android.app.ActionBar.TabListener;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.github.androidutils.logger.Logger;
+
 import de.unierlangen.like.R;
+import de.unierlangen.like.preferences.PreferenceWithHeaders;
 
 public class HelpActivity extends Activity implements TabListener {
 
     /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
+     * TODO finish comment The {@link android.support.v4.view.PagerAdapter} that
+     * will provide fragments for each of the sections. We use a
      * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
      * will keep every loaded fragment in memory. If this becomes too memory
      * intensive, it may be best to switch to a
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     SectionsPagerAdapter mSectionsPagerAdapter;
+    private final Logger log = Logger.getDefaultLogger();
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -33,8 +42,7 @@ public class HelpActivity extends Activity implements TabListener {
         setContentView(R.layout.help);
 
         // Create the adapter that will return a fragment for each of the three
-        // primary sections
-        // of the app.
+        // primary sections of the app.
         mSectionsPagerAdapter = new SectionsPagerAdapter();
 
         // Set up the action bar.
@@ -46,10 +54,8 @@ public class HelpActivity extends Activity implements TabListener {
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
         // When swiping between different sections, select the corresponding
-        // tab.
-        // We can also use ActionBar.Tab#select() to do this if we have a
-        // reference to the
-        // Tab.
+        // tab. We can also use ActionBar.Tab#select() to do this if we have a
+        // reference to the Tab.
         mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageSelected(int position) {
@@ -60,10 +66,9 @@ public class HelpActivity extends Activity implements TabListener {
         // For each of the sections in the app, add a tab to the action bar.
         for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
             // Create a tab with text corresponding to the page title defined by
-            // the adapter.
-            // Also specify this Activity object, which implements the
-            // TabListener interface, as the
-            // listener for when this tab is selected.
+            // the adapter. Also specify this Activity object, which implements
+            // the TabListener interface, as the listener for when this tab is
+            // selected.
             actionBar.addTab(actionBar.newTab().setText(mSectionsPagerAdapter.getPageTitle(i))
                     .setTabListener(this));
         }
@@ -76,24 +81,13 @@ public class HelpActivity extends Activity implements TabListener {
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
         // When the given tab is selected, switch to the corresponding page
-        // in
-        // the ViewPager.
+        // in the ViewPager.
         mViewPager.setCurrentItem(tab.getPosition());
     }
 
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
-
-    /*
-     * @Override public boolean onCreateOptionsMenu( Menu menu ) {
-     * getMenuInflater().inflate( R.menu.main, menu ); return true; }
-     * 
-     * @Override public boolean onOptionsItemSelected( MenuItem item ) { boolean
-     * ret; if (item.getItemId() == R.id.menu_settings) { // Handle Settings ret
-     * = true; } else { ret = super.onOptionsItemSelected( item ); } return ret;
-     * }
-     */
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -145,5 +139,35 @@ public class HelpActivity extends Activity implements TabListener {
         public int getCount() {
             return 2;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_help, menu);
+        return true;
+    }
+
+    /**
+     * override onOptions ItemSelected here
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.find_room:
+            startActivity(new Intent(this, FindRoomActivity.class));
+            break;
+        case R.id.about:
+            startActivity(new Intent(this, AboutActivity.class));
+            break;
+        case R.id.prefs:
+            startActivity(new Intent(this, PreferenceWithHeaders.class));
+            break;
+        default:
+            log.w("unexpected item " + item.getTitle());
+        }
+        // return super.onOptionsItemSelected(item);
+        return false;
     }
 }
